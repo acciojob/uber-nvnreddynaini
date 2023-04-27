c62162d173ff3a15965f207d3fb17fa17aed69d2
nvnreddynaini <nvnreddynaini@gmail.com>
2023-04-28 03:56:43 +0530

    5

--- src/main/java/com/driver/model/Customer.java
+++ src/main/java/com/driver/model/Customer.java
@@ -1,6 +1,7 @@
 package com.driver.model;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -16,7 +17,7 @@ public class Customer {
     private String password;
 
     @OneToMany(mappedBy = "customer",cascade = CascadeType.ALL)
-    private List<TripBooking> tripBookingList;
+    private List<TripBooking> tripBookingList = new ArrayList<>();
 
     public Customer() {
     }
--- src/main/java/com/driver/model/Driver.java
+++ src/main/java/com/driver/model/Driver.java
@@ -1,6 +1,7 @@
 package com.driver.model;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -20,7 +21,7 @@ public class Driver {
     private Cab cab;
 
     @OneToMany(mappedBy = "driver",cascade = CascadeType.ALL)
-    private List<TripBooking> tripBookingList;
+    private List<TripBooking> tripBookingList = new ArrayList<>();
 
     public Driver() {
     }
